17e955632425287a8bc3d84a36b6af6d8129b61a
Christopher Roach <vthakr@gmail.com>
2014-04-01 11:34:10 -0700

    Got fixtures working on the class level

--- flask_fixtures/__init__.py
+++ flask_fixtures/__init__.py
@@ -16,6 +16,7 @@ See the License for the specific language governing permissions and
 
 from __future__ import print_function
 
+import functools
 import importlib
 import inspect
 import os
@@ -108,11 +109,11 @@ def load_fixtures(db, fixtures):
     else:
       raise ValueError("Fixture missing a 'model' or 'table' field: %s" % json.dumps(fixture))
 
+
 CLASS_SETUP_NAMES = ('setUpClass', 'setup_class', 'setup_all', 'setupClass', 'setupAll', 'setUpAll')
 CLASS_TEARDOWN_NAMES = ('tearDownClass', 'teardown_class', 'teardown_all', 'teardownClass', 'teardownAll', 'tearDownAll')
 TEST_SETUP_NAMES = ('setUp',)
 TEST_TEARDOWN_NAMES = ('tearDown',)
-
 
 
 class Fixtures(object):
@@ -146,17 +147,41 @@ class Fixtures(object):
         filepath = os.path.join(directory, filename)
         if os.path.exists(filepath):
           # TODO load the data into the database
-          load_fixtures(self.db, load_file(filepath))
+          self.load_fixtures(self.load_file(filepath))
           break
       else:
         # TODO should we raise an error here instead?
         print("Error loading '%s'. File could not be found." % filename, file=sys.stderr)
 
-
   def teardown(self):
     print("tearing down fixtures...")
     self.db.session.expunge_all()
     self.db.drop_all()
+
+  def load_file(self, filename):
+    """Returns list of fixtures from the given file.
+    """
+    name, extension = os.path.splitext(filename)
+    if extension.lower() in ('.yaml', '.yml'):
+      if not YAML_INSTALLED:
+        raise Exception("Could not load fixture '%s'; PyYAML must first be installed")
+      loader = yaml.load
+    elif extension.lower() in ('.json', '.js'):
+      loader = json.load
+    else:
+      # Try both supported formats
+      def loader(f):
+        try:
+          return yaml.load(f)
+        except Exception:
+          pass
+        try:
+          return json.load(f)
+        except Exception:
+          pass
+        raise Exception("Could not load fixture '%s'; unsupported format")
+    with open(filename, 'r') as fin:
+      return loader(fin)
 
   def load_fixtures(self, fixtures):
     """Loads the given fixtures into the database.
@@ -179,17 +204,56 @@ class Fixtures(object):
       else:
         raise ValueError("Fixture missing a 'model' or 'table' field: %s" % json.dumps(fixture))
 
+  def wrap_method(self, method, fixtures):
+    """Wraps a method in a set of fixtures setup/teardown functions.
+    """
+    def wrapper(*args, **kwargs):
+      self.setup(fixtures)
+      try:
+        method(*args, **kwargs)
+      finally:
+        self.teardown()
+    functools.update_wrapper(wrapper, method)
+    return wrapper
+
+  def wrap_class(self, cls, fixtures):
+    """Adds fixtures setup/teardown methods at the class level.
+
+    This decorator piggybacks on the setUpClass, tearDownClass methods that
+    the unittest/unittest2/nose packages call upon class creation and after
+    all tests in the class have finished running.
+
+    """
+    def wrap_method(cls, fixtures_fn, names):
+      methods = filter(None, [getattr(cls, name, None) for name in names])
+      if len(methods) > 1:
+        raise RuntimeError("Cannot have more than one setup/teardown method, found %s" %
+          ', '.join(fn.__name__ for fn in methods))
+      elif len(methods) == 1:
+        wrapped_method = methods[0]
+        def wrapper(cls, *args, **kwargs):
+          fixtures_fn()
+          wrapped_method(*args, **kwargs)
+        functools.update_wrapper(wrapper, wrapped_method)
+        setattr(cls, wrapper.__name__, classmethod(wrapper))
+      else:
+        def wrapper(cls, *args, **kwargs):
+          fixtures_fn()
+          setattr(cls, 'setup_class', classmethod(wrapper))
+
+    wrap_method(cls, lambda: self.setup(fixtures), CLASS_SETUP_NAMES)
+    wrap_method(cls, lambda: self.teardown(), CLASS_TEARDOWN_NAMES)
+
+    return cls
 
   def __call__(self, *fixtures):
-    def decorator(fn):
-      def wrapper(*args, **kwargs):
-        self.setup(fixtures)
-        try:
-          fn(*args, **kwargs)
-        finally:
-          self.teardown()
-      wrapper.__name__ = fn.__name__
-      return wrapper
+    def decorator(obj):
+      if inspect.isfunction(obj):
+        return self.wrap_method(obj, fixtures)
+      elif inspect.isclass(obj):
+        return self.wrap_class(obj, fixtures)
+      else:
+        raise TypeError("received an object of type '%s' expected 'function' or 'classobj'" % type(obj))
     return decorator
 
 
