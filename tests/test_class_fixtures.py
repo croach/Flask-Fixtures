963d7d2c835a1b6deb1daa7605aa121054200437
Christopher Roach <vthakr@gmail.com>
2015-06-21 20:19:45 -0700

    Making sure the class_fixtures test only runs for python 2.7+

--- tests/test_class_fixtures.py
+++ tests/test_class_fixtures.py
@@ -1,3 +1,5 @@
+import datetime
+import sys
 import unittest
 
 from myapp import app
@@ -12,42 +14,59 @@ app.config.from_object('myapp.config.TestConfig')
 FixturesMixin.init_app(app, db)
 
 
-class TestClassFixtures(unittest.TestCase, FixturesMixin):
+# The setUpClass and tearDownClass methods were added to unittest.TestCase in
+# python 2.7, so if we're running a version of python below that this test
+# will fail. So, make sure we only run the test with python 2.7 or greater.
+if sys.hexversion >= 0x02070000:
 
-    # Specify the fixtures file(s) you want to load
-    class_fixtures = ['authors.json']
+    class TestClassFixtures(unittest.TestCase, FixturesMixin):
 
-    # Your tests go here
+        # Specify the fixtures file(s) you want to load
+        class_fixtures = ['authors.json']
 
-    @classmethod
-    def setUpClass(self):
-        # Make sure that the user defined setUp method runs after the fixtures
-        # setup function (i.e., the database should be setup already)
-        assert Author.query.count() == 1
-        assert Book.query.count() == 3
+        @classmethod
+        def setUpClass(self):
+            # Make sure we start out with 1 author and 3 books
+            assert Author.query.count() == 1
+            assert Book.query.count() == 3
 
-    @classmethod
-    def tearDownClass(self):
-        # This should run before the fixtures tear down function, so the
-        # database should still contain all the fixtures data
-        assert Author.query.count() == 3
-        assert Book.query.count() == 3
+        @classmethod
+        def tearDownClass(self):
+            # Since we never tore down the DB in between tests, we should have 3
+            # authors and 5 books now (the initial fixtures plus the records the
+            # tests added).
+            assert Author.query.count() == 3
+            assert Book.query.count() == 5
 
-    def test_one(self):
-        print "Inside test_one"
-        # Add another author on the fly
-        author = Author()
-        author.first_name = 'George'
-        author.last_name = 'Orwell'
-        self.db.session.add(author)
-        self.db.session.commit()
+        def test_one(self):
+            print "Inside test_one"
+            # Add another author on the fly
+            author = Author()
+            author.first_name = 'George'
+            author.last_name = 'Orwell'
+            self.db.session.add(author)
 
-    def test_two(self):
-        print "Inside test_two"
-        # Add another author on the fly
-        author = Author()
-        author.first_name = 'Aldous'
-        author.last_name = 'Huxley'
-        self.db.session.add(author)
-        self.db.session.commit()
+            # Add another book for the new author
+            book = Book()
+            book.title = "1984"
+            book.published_date = datetime.datetime(1949, 6, 8)
+            self.db.session.add(book)
+
+            self.db.session.commit()
+
+        def test_two(self):
+            print "Inside test_two"
+            # Add another author on the fly
+            author = Author()
+            author.first_name = 'Aldous'
+            author.last_name = 'Huxley'
+            self.db.session.add(author)
+
+            # Add another book for the new author
+            book = Book()
+            book.title = "Brave New World"
+            book.published_date = datetime.datetime(1932, 5, 12)
+            self.db.session.add(book)
+
+            self.db.session.commit()
 
